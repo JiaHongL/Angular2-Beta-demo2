5acb7d61a3ad2de4914ba253a4b11fb6c521cefb
JiaHongL <jiahongl@jiahongLdeMacBook-Pro.local>
2016-05-12 02:05:35 +0800

    add enableProdMode

--- app/main.ts
+++ app/main.ts
@@ -3,6 +3,9 @@ import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
 import { provide }        from '@angular/core';
 import { LocationStrategy,HashLocationStrategy } from '@angular/common';
 import { HTTP_PROVIDERS } from '@angular/http';
+import {enableProdMode} from '@angular/core/index';
+
+enableProdMode();
 
 import {AppComponent}     from './app.component';
 
