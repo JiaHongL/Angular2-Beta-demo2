0c5f054ebec218468a6a827658ba53c0bd47c33c
JiaHongL <jiahongl@jiahongLdeMacBook-Pro.local>
2016-05-12 02:08:37 +0800

    修改 enableProdMode() 位置

--- app/main.ts
+++ app/main.ts
@@ -4,10 +4,8 @@ import { provide }        from '@angular/core';
 import { LocationStrategy,HashLocationStrategy } from '@angular/common';
 import { HTTP_PROVIDERS } from '@angular/http';
 import {enableProdMode} from '@angular/core/index';
-
-enableProdMode();
-
 import {AppComponent}     from './app.component';
 
+enableProdMode();
 bootstrap(AppComponent, [ROUTER_PROVIDERS,
           provide(LocationStrategy, {useClass: HashLocationStrategy}),HTTP_PROVIDERS]);
